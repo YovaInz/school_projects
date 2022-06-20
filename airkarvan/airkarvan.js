7a2390e3bb6fd33dd0b047c2213b24e34a0273cb
YovaInz <94088773+YovaInz@users.noreply.github.com>
2022-06-20 12:45:01 -0600

    Add files via upload

---
+++ airkarvan/airkarvan.js
@@ -0,0 +1,37 @@
+$(document).ready(function($){
+
+    $("#i1").click(function(){
+        document.getElementById('i1').classList.add("ui-btn-active");
+    });
+
+    $("#v1").click(function (){
+        document.getElementById('v2').classList.add("ui-btn-active");
+    });
+
+    $("#p1").click(function (){
+        document.getElementById('p3').classList.add("ui-btn-active");
+    });
+    $("#i2").click(function(){
+        document.getElementById('i1').classList.add("ui-btn-active");
+    });
+
+    $("#v2").click(function (){
+        document.getElementById('v2').classList.add("ui-btn-active");
+    });
+
+    $("#p2").click(function (){
+        document.getElementById('p3').classList.add("ui-btn-active");
+    });
+    $("#i3").click(function(){
+        document.getElementById('i1').classList.add("ui-btn-active");
+    });
+
+    $("#v3").click(function (){
+        document.getElementById('v2').classList.add("ui-btn-active");
+    });
+
+    $("#p3").click(function (){
+        document.getElementById('p3').classList.add("ui-btn-active");
+    });
+
+});
